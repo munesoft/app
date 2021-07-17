10242a95c130591bd66fce9d8f2e3f9b742bcc28
munesoft <mullageeks@gmail.com>
2021-07-17 20:33:52 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading style="font-style: italic; color: green; text-align: center;">Thanks For Installing Muneship - Order Fufillment app!💃🕺🎉 ..you will be redirected to your dashboard shortly. </Heading>
+  </Page>
+);
+
+export default Index;
